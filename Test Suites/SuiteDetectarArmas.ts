a11c6afc82c0eac64ca5bc87faae3b42a8536eed
ayrto <ayrto@192.168.56.1>
2022-07-16 18:38:33 -0500

    Se agrego el suite case para la funcionalidad detectar arma

---
+++ Test Suites/SuiteDetectarArmas.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SuiteDetectarArmas</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6ec9e263-3676-44a8-b269-5a5298fec0dc</testSuiteGuid>
+   <testCaseLink>
+      <guid>883de450-65bd-4ae9-b16b-dde62b833244</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DetectArmas</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
