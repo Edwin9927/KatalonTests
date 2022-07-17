1d2b5ab5d5efe4fb0182d300f577dd17ff926ae4
AyrtonFidel9 <ayrtonfidel9@gmail.com>
2022-07-17 15:35:54 -0500

    test v2

--- Test Suites/SuiteCreatePoseNPos.ts
+++ Test Suites/SuiteCreatePoseNPos.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>5e25ffa1-2b8e-4bee-ad70-c7a0cc2254db</testSuiteGuid>
+   <testCaseLink>
+      <guid>2750023b-892b-44f3-94bf-b1901c649474</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Func-04-CreatePosNPos</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
