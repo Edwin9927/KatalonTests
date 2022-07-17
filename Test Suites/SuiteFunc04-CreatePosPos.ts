e4e4cf88d4da85ca5855c091654368063b2f85fb
AyrtonFidel9 <ayrtonfidel9@gmail.com>
2022-07-17 13:22:00 -0500

    Test FUN 04
    Test realizados para la funcionalidad 04, para la creacion de
    las posiciones de las imagenes positivas del entrenamiento

---
+++ Test Suites/SuiteFunc04-CreatePosPos.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SuiteFunc04-CreatePosPos</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cbb38668-8473-4460-988b-6e857256261a</testSuiteGuid>
+</TestSuiteEntity>
