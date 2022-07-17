fbe3f168a1403c8aff49230dbee0c1de1f94bd7f
Carolina4-lang <carolinacastillo1d@gmail.com>
2022-07-17 18:37:42 -0500

    Pruebas_Caro

---
+++ Test Suites/SuiteTestStorePositive.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Test de store positive images</description>
+   <name>SuiteTestStorePositive</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0a19e68d-9faf-431d-bb68-05efe37487cc</testSuiteGuid>
+</TestSuiteEntity>
