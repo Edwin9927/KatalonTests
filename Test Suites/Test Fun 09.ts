ca75219414d3ec3feb8cdff710b1cdceeba0ffa4
Edwin9927 <edwinmanzanoquinzo@gmail.com>
2022-07-16 23:56:30 -0500

    Primer caso de prueba de la funcionalidad 09

---
+++ Test Suites/Test Fun 09.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Fun 09</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>da0b9f28-27aa-4190-9570-3c9025f90eeb</testSuiteGuid>
+   <testCaseLink>
+      <guid>6bf3cf39-546c-4865-850f-7d1f3d8e223c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case - Iniciar reconocimiento</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
