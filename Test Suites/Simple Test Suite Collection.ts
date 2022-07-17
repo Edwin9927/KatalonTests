3c6e29b87ae249dd1c3e67184595d4ff71be3966
AyrtonFidel9 <ayrtonfidel9@gmail.com>
2022-07-17 15:23:23 -0500

    Test Realizados para la funcionalidad 4
    Test realizados para la funcionalidad de crear las posiciones de las imagenes
    positivas

--- Test Suites/Simple Test Suite Collection.ts
+++ Test Suites/Simple Test Suite Collection.ts
@@ -17,5 +17,15 @@
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/SuiteDetectarArmas</testSuiteEntity>
       </TestSuiteRunConfiguration>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Chrome</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/SuiteCreatePoseNPos</testSuiteEntity>
+      </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
 </TestSuiteCollectionEntity>
---
+++ Test Suites/SuiteCreatePoseNPos.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SuiteCreatePoseNPos</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5e25ffa1-2b8e-4bee-ad70-c7a0cc2254db</testSuiteGuid>
+</TestSuiteEntity>
