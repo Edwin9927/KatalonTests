e70aa35d33f95f05b4d205b9e124a7da956b0605
SBola97 <sebassenk1997@gmail.com>
2022-07-17 20:09:40 -0500

    añadida funcionalidad de CreatePosNNeg

--- Test Suites/Simple Test Suite Collection.ts
+++ Test Suites/Simple Test Suite Collection.ts
@@ -27,5 +27,15 @@
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/SuiteCreatePoseNPos</testSuiteEntity>
       </TestSuiteRunConfiguration>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Chrome</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/SuiteCreatePosNeg</testSuiteEntity>
+      </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
 </TestSuiteCollectionEntity>
