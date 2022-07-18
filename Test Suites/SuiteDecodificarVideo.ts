06f5c0e1cc238e27354d0ff5ae89c96d77089209
AyrtonFidel9 <ayrtonfidel9@gmail.com>
2022-07-17 19:49:11 -0500

    collection agregado

--- Test Suites/SuiteDecodificarVideo.ts
+++ Test Suites/SuiteDecodificarVideo.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>19895c99-5783-4f5c-8473-3cb2cc18c300</testSuiteGuid>
+   <testCaseLink>
+      <guid>08b0ea9b-2317-464b-8406-a36a364e884b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DecodificarVideoTestCase</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
